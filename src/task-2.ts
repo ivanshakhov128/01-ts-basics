7374fefcd9789fc9d51821bc45f959e3ceff4735
Ivan Shakhov <ivanshahov04@gmail.com>
2025-10-24 19:11:55 +0300

    changes

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,10 +1,10 @@
-interface ProductType {
+interface Product {
   readonly id: number;
   title: string;
   description?: string;
 }
 
-const product: ProductType = {
+const product: Product = {
   id: 1,
   title: "Tablet",
   description: "Compact and fast",
--- src/task-3.ts
+++ src/task-3.ts
@@ -1,4 +1,4 @@
-interface ProductType {
+interface Product {
   id: number;
   title: string;
 }
@@ -7,7 +7,7 @@ const usernames: string[] = ["alice", "bob", "charlie"];
 
 const ratings: number[] = [4.5, 3.8, 5];
 
-const products: ProductType[] = [
+const products: Product[] = [
   { id: 1, title: "Phone" },
   { id: 2, title: "Laptop" },
 ];
